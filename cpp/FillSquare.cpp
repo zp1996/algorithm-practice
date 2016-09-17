08e65e8aa48239eb91256df9f0071dd345b7c615
周鹏 <1179258516@qq.com>
2016-09-17 21:02:09 +0800

    update cpp

--- cpp/FillSquare.cpp
+++ cpp/FillSquare.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 const int size = 8;
 int matrix[size][size];
-int c = 1;
+int c = 0;
 
 void fill (int tr, int tc, int dr, int dc, int s)
 {
@@ -22,34 +22,34 @@ void fill (int tr, int tc, int dr, int dc, int s)
     if (dr < tr + half && dc < tc + half) {
         fill(tr, tc, dr, dc, half);
     } else {  // 无特殊位置,标记右下角为特殊位置
-        matrix[tr + s - 1][tc + s - 1] = 1;
-        fill(tr, tc, tr + s - 1, tc + s - 1, half);
+        matrix[tr + half - 1][tc + half - 1] = 1;
+        fill(tr, tc, tr + half - 1, tc + half - 1, half);
     }
     // 右上角
-    if (dr < tr + s && dc >= tc + s) {
-        fill(tr, tc + s, dr, dc, half);
+    if (dr < tr + half && dc >= tc + half) {
+        fill(tr, tc + half, dr, dc, half);
     } else {
-        matrix[tr + s - 1][tc + s] = 1;
-        fill(tr, tc + s, tr + s - 1, tc + s, half);
+        matrix[tr + half - 1][tc + half] = 1;
+        fill(tr, tc + half, tr + half - 1, tc + half, half);
     }
     // 左下角
-    if (dr >= tr + s && dc < tc + s) {
-        fill(tr + s, tc, dr, dc, half);
+    if (dr >= tr + half && dc < tc + half) {
+        fill(tr + half, tc, dr, dc, half);
     } else {
-        matrix[tr + s][tc + s - 1] = 1;
-        fill(tr + s, tc, tr + s, tc + s - 1, half);
+        matrix[tr + half][tc + half - 1] = 1;
+        fill(tr + half, tc, tr + half, tc + half - 1, half);
     }
     // 右下角
-    if (dr >= tr + s && dc >= tc + s) {
-        fill(tr + s, tc + s, dr, dc, half);
+    if (dr >= tr + half && dc >= tc + half) {
+        fill(tr + half, tc + half, dr, dc, half);
     } else {
-        matrix[tr + s][tc + s] = 1;
-        fill(tr + s, tc + s, tr + s, tc + s, half);
+        matrix[tr + half][tc + half] = 1;
+        fill(tr + half, tc + half, tr + half, tc + half, half);
     }
 }
 int main ()
 {
     fill(0, 0, 1, 1, size);
-    cout<<"使棋盘满了需要："<<c - 1<<"块L型"<<endl;
+    cout<<"使棋盘满了需要："<<c<<"块L型"<<endl;
     return 0;
 }
