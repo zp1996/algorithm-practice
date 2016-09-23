299cd72cf6781412c61f3a394ede14a96ede4956
周鹏 <1179258516@qq.com>
2016-09-23 21:19:46 +0800

    update lru

---
+++ src/js/lru.js
@@ -0,0 +1,74 @@
+var elements = Array.from(document.getElementsByClassName("stack"));
+console.log(elements);
+function Cache (limit) {
+  this.size = 0;
+  this.limit = limit;
+  this.head = this.tail = undefined;
+  this._keymap = Object.create(null);
+}
+
+var p = Cache.prototype;
+
+p.put = function (key, value) {
+  var removed;
+
+  var entry = this.get(key, true);
+  if (!entry) {
+    if (this.size === this.limit) {
+      removed = this.shift();
+    }
+    entry = {
+      key: key
+    };
+    this._keymap[key] = entry;
+    if (this.tail) {
+      this.tail.newer = entry;
+      entry.older = this.tail;
+    } else {
+      this.head = entry;
+    }
+    this.tail = entry;
+    this.size++;
+  }
+  entry.value = value;
+
+  return removed;
+};
+
+p.shift = function () {
+  var entry = this.head;
+  if (entry) {
+    this.head = this.head.newer;
+    this.head.older = undefined;
+    entry.newer = entry.older = undefined;
+    this._keymap[entry.key] = undefined;
+    this.size--;
+  }
+  return entry;
+};
+
+p.get = function (key, returnEntry) {
+  var entry = this._keymap[key]
+  if (entry === undefined) return
+  if (entry === this.tail) {
+    return returnEntry
+      ? entry
+      : entry.value
+  }
+  if (entry.newer) {
+    if (entry === this.head) {
+      this.head = entry.newer;
+    }
+    entry.newer.older = entry.older;
+  }
+  if (entry.older) {
+    entry.older.newer = entry.newer;
+  }
+  entry.newer = undefined;
+  entry.older = this.tail;
+  if (this.tail) {
+    this.tail.newer = entry;
+  }
+  this.tail = entry;
+  return returnEntry ? entry : entry.value;
+};
